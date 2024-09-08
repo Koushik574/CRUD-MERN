f4b2b9b98e7a412dfb4c8e6db2aecb3c34cca8c9
Sai Koushik <koushiksai574@gmail.com>
2024-09-08 22:57:41 +0530

    Initial commit

---
+++ Client/src/Users.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/jsx-key */
+import { Link } from "react-router-dom";
+
+import { useEffect, useState } from "react"
+
+import axios from "axios"
+
+const Users = () => {
+
+    // eslint-disable-next-line no-unused-vars
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:3000")
+        .then(result => setUsers(result.data))
+        .catch(err => console.log(err))
+    }, [])
+
+    const handleDelete = (id) => {
+        axios.delete("http://localhost:3000/deleteUser/" + id)
+        .then(res => {console.log(res)
+            window.location.reload()
+        })
+        .catch(err => console.log(err))
+    }
+
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+        <div className="w-50 bg-white rounded p-3">
+            <Link to="/create" className="btn btn-success">Add +</Link>
+            <table className="table">
+
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        users.map((e) => {
+                            
+                            return(<tr key={e._id}>
+                                <td>{e.name}</td>
+                                <td>{e.email}</td>
+                                <td>{e.age}</td>
+                                <td>
+                                <Link to={`/update/${e._id}`} className="btn btn-success">Edit</Link>
+                                <button className="btn btn-danger" onClick={() => handleDelete(e._id)}>Delete</button>
+                                </td>
+                            </tr>)
+                        })
+                    }
+
+                </tbody>
+
+            </table>
+        </div>
+
+    </div>
+  )
+}
+
+export default Users
---
+++ Server/index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const UserModel = require("./models/Users")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://root:1234@cluster-1.onphs.mongodb.net/crud")
+
+app.get("/", (req, res) => {
+    UserModel.find({})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.get("/getUser/:id", (req, res) => {
+    const id = req.params.id
+    UserModel.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.post("/addUser", (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.put("/updateUser/:id", (req, res) => {
+    const id = req.params.id
+    UserModel.findByIdAndUpdate({_id:id}, {name: req.body.name, email: req.body.email, age: req.body.age})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.delete("/deleteUser/:id", (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id: id})
+      .then((result) => res.json(result))
+      .catch((err) => res.json(err));
+  });
+
+app.listen(3000, () =>{
+    console.log("Server is running in port 3000")
+})
