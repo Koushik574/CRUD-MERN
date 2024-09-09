8400cb645f2e94fcb1bafa61f3818da08e8d10bd
Sai Koushik <koushiksai574@gmail.com>
2024-09-09 09:15:25 +0530

    Fixed errors

--- Server/index.js
+++ Server/index.js
@@ -1,61 +1,56 @@
 require('dotenv').config();
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/Users");
 
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const UserModel = require("./models/Users")
+const app = express();
 
-const app = express()
-
-app.use(cors({ origin: ["http://localhost:5173", "https://crud-mern-xi-five.vercel.app/"] }))
-app.use(express.json())
-
-const mongoose = require('mongoose');
+app.use(cors({ origin: ["http://localhost:5173", "https://crud-mern-xi-five.vercel.app/"] }));
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-
 app.get("/", (req, res) => {
     UserModel.find({})
     .then(users => res.json(users))
-    .catch(err => res.json(err))
-})
+    .catch(err => res.json(err));
+});
 
 app.get("/getUser/:id", (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
     UserModel.findById({_id:id})
-    .then(users => res.json(users))
-    .catch(err => res.json(err))
-})
+    .then(user => res.json(user))
+    .catch(err => res.json(err));
+});
 
 app.post("/addUser", (req, res) => {
     UserModel.create(req.body)
-    .then(users => res.json(users))
-    .catch(err => res.json(err))
-})
+    .then(user => res.json(user))
+    .catch(err => res.json(err));
+});
 
 app.put("/updateUser/:id", (req, res) => {
-    const id = req.params.id
-    UserModel.findByIdAndUpdate({_id:id}, {name: req.body.name, email: req.body.email, age: req.body.age})
-    .then(users => res.json(users))
-    .catch(err => res.json(err))
-})
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id:id}, {name: req.body.name, email: req.body.email, age: req.body.age}, { new: true })
+    .then(user => res.json(user))
+    .catch(err => res.json(err));
+});
 
 app.delete("/deleteUser/:id", (req, res) => {
     const id = req.params.id;
     UserModel.findByIdAndDelete({_id: id})
-      .then((result) => res.json(result))
-      .catch((err) => res.json(err));
-  });
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-  });
-  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
